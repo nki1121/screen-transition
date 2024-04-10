dc6423c2ea1fb44676aa619a4547f2fa5fc58846
nki1121 <nkiuniv@gmail.com>
2024-04-11 00:07:24 +0900

    Initial Commit

---
+++ screent-transition/screent_transitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  screent_transitionApp.swift
+//  screent-transition
+//
+//  Created by 中井晋平 on 2024/04/11.
+//
+
+import SwiftUI
+
+@main
+struct screent_transitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
